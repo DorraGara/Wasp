d607762f095466a2000f606ca2f614d6e94e5e27
DorraGara <dorra.gara@insat.u-carthage.tn>
2022-01-19 14:55:59 +0100

    menu and functionnalities templates

--- routes/routes.py
+++ routes/routes.py
@@ -29,3 +29,84 @@ def registerScreen():
 @app.route('/loginScreen')
 def loginScreen():
   return render_template('home.html')
+
+# Menus
+@app.route('/dashboard/codage')
+def codageMenuScreen():
+  return render_template('Menus/codageMenu.html')
+
+@app.route('/dashboard/hashing')
+def hashingMenuScreen():
+  return render_template('Menus/hashingMenu.html')
+
+@app.route('/dashboard/cracking')
+def crackingAlgoMenuScreen():
+  return render_template('Menus/crackingMenuAlgo.html')
+
+@app.route('/dashboard/codage/encoding')
+def encodingScreen():
+  return render_template('Functions/Encoding/encoding.html')
+
+@app.route('/dashboard/codage/decoding')
+def decodingScreen():
+  return render_template('Functions/Encoding/decoding.html')
+
+@app.route('/dashboard/hashing/algorithm')
+def hashingScreen():
+  algo = request.args.get('algo')
+  return render_template('Functions/hashing.html', algo = algo)
+
+@app.route('/dashboard/cracking/algorithm')
+def crackingDictionnaryMenuScreen():
+  algo = request.args.get('algo')
+  return render_template('Menus/crackingMenuWordlist.html', algo = algo)
+
+@app.route('/dashboard/cracking/dictionnary')
+def crackingScreen():
+  algo = request.args.get('algo')
+  dict = request.args.get('dictionnary')
+  print(dict)
+  return render_template('Functions/cracking.html', algo=algo, dictionnary =dict)
+
+@app.route('/dashboard/symmetric')
+def symmetricAlgoMenuScreen():
+  return render_template('Menus/symmetricEncryptionAlgoMenu.html')
+
+@app.route('/dashboard/symmetric/algorithm')
+def symmetricEndecMenuScreen():
+  algo = request.args.get('algo')
+  return render_template('Menus/symmetricEncryptionMenu.html', algo = algo)
+
+@app.route('/dashboard/symmetric/encryption')
+def symmetricEncryptionMenuScreen():
+  algo = request.args.get('algo')
+  return render_template('Functions/symmetricEncryption/encrypting.html', algo = algo)
+
+@app.route('/dashboard/symmetric/decryption')
+def symmetricDecryptionMenuScreen():
+  algo = request.args.get('algo')
+  return render_template('Functions/symmetricEncryption/decrypting.html', algo = algo)
+
+@app.route('/dashboard/asymmetric')
+def asymmetricAlgoMenuScreen():
+  return render_template('Menus/asymmetricEncryptionAlgoMenu.html')
+
+@app.route('/dashboard/asymmetric/algorithm')
+def asymmetricEndecMenuScreen():
+  algo = request.args.get('algo')
+  return render_template('Menus/asymmetricEncryptionMenu.html', algo = algo)
+
+@app.route('/dashboard/asymmetric/encryption')
+def asymmetricEncryptionMenuScreen():
+  algo = request.args.get('algo')
+  return render_template('Functions/asymmetricEncryption/encrypting.html', algo = algo)
+
+@app.route('/dashboard/asymmetric/decryption')
+def asymmetricDecryptionMenuScreen():
+  algo = request.args.get('algo')
+  return render_template('Functions/asymmetricEncryption/decrypting.html', algo = algo)
+
+@app.route('/dashboard/asymmetric/keygen')
+def asymmetricKeygenMenuScreen():
+  algo = request.args.get('algo')
+  return render_template('Functions/asymmetricEncryption/generatingKeys.html', algo = algo)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -51,3 +51,161 @@ $("#signout").click(function(e){
     },
   });
 });
+
+// Menus
+
+
+$("form[name=mainMenu").submit(function(e) {
+  var option = $("input[name=option").val();
+  switch(option) {
+          case "1":
+            window.location.assign("/dashboard/codage");
+            break;
+          case "2":
+            window.location.assign("/dashboard/hashing");
+            break;
+          case "3":
+            window.location.assign("/dashboard/cracking");
+          break;
+          case "4":
+            window.location.assign("/dashboard/symmetric");
+          break;
+          case "5":
+            window.location.assign("/dashboard/asymmetric");
+          break;
+          case "6":
+            window.location.assign("/dashboard/chatroom");
+          break;
+          default:
+            window.location.assign("/dashboard");
+          }
+  e.preventDefault();
+});
+
+$("form[name=codageMenu").submit(function(e) {
+  var option = $("input[name=option").val();
+  switch(option) {
+    case "1":
+      window.location.assign("/dashboard/codage/encoding");
+      break;
+    case "2":
+      window.location.assign("/dashboard/codage/decoding");
+      break;
+    case "0":
+      window.location.assign("/dashboard");
+    break;
+    default:
+      window.location.assign("/dashboard/codage");
+    }
+  e.preventDefault();
+});
+
+$("form[name=crackingAlgoMenu").submit(function(e) {
+  e.preventDefault();
+  var algorithm = $("input[name=option").val();
+  if (algorithm == "0") {
+    window.location.assign("/dashboard");
+  } else {
+    window.location.assign("/dashboard/cracking/algorithm?algo="+algorithm);
+  }
+});
+$("form[name=crackingWordlistMenu").submit(function(e) {
+  e.preventDefault();
+  var dictionnary = $("input[name=dictionnary").val();
+  var algorithm = $("input[name=algo").val();
+  if (dictionnary == "0") {
+    window.location.assign("/dashboard");
+  } else {
+    window.location.assign("/dashboard/cracking/dictionnary?algo="+algorithm+"&dictionnary="+dictionnary);
+  }
+});
+
+$("form[name=symmetricEncryptionAlgoMenu").submit(function(e) {
+  e.preventDefault();
+  var algorithm = $("input[name=option").val();
+  if (algorithm == "0") {
+    window.location.assign("/dashboard");
+  } else {
+    window.location.assign("/dashboard/symmetric/algorithm?algo="+algorithm);
+  }
+});
+$("form[name=symmetricEnDecMenu").submit(function(e) {
+  e.preventDefault();
+  var endec = $("input[name=endec").val();
+  var algorithm = $("input[name=algo").val();
+  switch (endec) {
+    case "0":
+      window.location.assign("/dashboard");
+      break;
+    case "1":
+      window.location.assign("/dashboard/symmetric/encryption?algo="+algorithm);
+      break;
+    case "2":
+      window.location.assign("/dashboard/symmetric/decryption?algo="+algorithm);
+      break;
+    default:
+      //error
+      window.location.assign("/dashboard");
+  }
+});
+$("form[name=hashingMenu").submit(function(e) {
+  e.preventDefault();
+  var algorithm = $("input[name=option").val();
+  if (algorithm == "0") {
+    window.location.assign("/dashboard");
+  } else {
+    window.location.assign("/dashboard/hashing/algorithm?algo="+algorithm);
+  }
+});
+
+$("form[name=asymmetricEncryptionAlgoMenu").submit(function(e) {
+  e.preventDefault();
+  var algorithm = $("input[name=option").val();
+  if (algorithm == "0") {
+    window.location.assign("/dashboard");
+  } else {
+    window.location.assign("/dashboard/asymmetric/algorithm?algo="+algorithm);
+  }
+});
+$("form[name=asymmetricEnDecMenu").submit(function(e) {
+  e.preventDefault();
+  var endec = $("input[name=endec").val();
+  var algorithm = $("input[name=algo").val();
+  switch (endec) {
+    case "0":
+      window.location.assign("/dashboard");
+      break;
+    case "1":
+        window.location.assign("/dashboard/asymmetric/keygen?algo="+algorithm);
+        break;
+    case "2":
+      window.location.assign("/dashboard/asymmetric/encryption?algo="+algorithm);
+      break;
+    case "3":
+      window.location.assign("/dashboard/asymmetric/decryption?algo="+algorithm);
+      break;
+    default:
+      //error
+      window.location.assign("/dashboard");
+  }
+});
+$(".closeCodage").click(function(e){
+  e.preventDefault();
+  window.location.assign("/dashboard/codage");
+});
+$(".closeHashage").click(function(e){
+  e.preventDefault();
+  window.location.assign("/dashboard/hashing");
+});
+$(".closeCraquage").click(function(e){
+  e.preventDefault();
+  window.location.assign("/dashboard/cracking");
+});
+$(".closeSymmetricEncryption").click(function(e){
+  e.preventDefault();
+  window.location.assign("/dashboard/symmetric");
+});
+$(".closeAsymmetricEncryption").click(function(e){
+  e.preventDefault();
+  window.location.assign("/dashboard/asymmetric");
+});
